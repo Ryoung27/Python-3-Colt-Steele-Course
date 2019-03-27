15c6de86ddfb237fc733c1955b1c9abf0828d3b9
Ryoung27 <RRYoung89@gmail.com>
2019-03-26 19:43:50 -0500

    more math

--- math/numbs.py
+++ math/numbs.py
@@ -2,3 +2,5 @@ print( 5*5*5)
 print( 5.0*5*5)
 print(type(1.9))
 print(10//3)
+
+"Math is pretty basic, but it's important. There's a million ways to do things, but you need to find the best that works for the current situation. If anyone is looking at this, I just really want to nail the basics and go back because practice makes perfect."

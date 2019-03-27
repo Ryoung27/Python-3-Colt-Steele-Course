27e4bddeb5570d2e34959425aa6469e2a7fedb1d
Ryoung27 <RRYoung89@gmail.com>
2019-03-26 20:05:02 -0500

    loops

--- math/numbs.py
+++ math/numbs.py
@@ -4,3 +4,25 @@ print(type(1.9))
 print(10//3)
 
 "Math is pretty basic, but it's important. There's a million ways to do things, but you need to find the best that works for the current situation. If anyone is looking at this, I just really want to nail the basics and go back because practice makes perfect."
+
+
+
+for x in range(10):
+    print(x)
+
+i = 0
+
+while i < 9:
+    print( i )
+    i = i + 1
+
+fruits = ["apple", "banana", "Oh my"]
+for x in fruits:
+    print (x)
+    if x == "banana":
+        print("NO")
+    else:
+        print('green')
+
+for x in "red":
+    print(x)

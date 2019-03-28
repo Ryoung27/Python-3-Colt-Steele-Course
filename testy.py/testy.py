c74d3bdd5d4713954ecdee844dd8a2b29c2d5930
Ryoung27 <RRYoung89@gmail.com>
2019-03-28 07:09:01 -0500

    Oh no, an e

---
+++ testy.py/testy.py
@@ -0,0 +1,12 @@
+test_var = "onetwothree"
+
+print(test_var[-1:])
+
+for i in test_var:
+    print(i)
+
+for i in test_var:
+    if i == "e":
+        print("Oh no, an e")
+    else:
+        print(i)

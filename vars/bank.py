4d47cea16738939a43ab120fb04bf09823d14726
Ryoung27 <RRYoung89@gmail.com>
2019-03-27 20:06:57 -0500

    functions

--- vars/bank.py
+++ vars/bank.py
@@ -1,15 +1,59 @@
-cash = 19867324678987.99
+# cash = 19867324678987.99
 
-print(cash/5)
+# print(cash/5)
 
-x = 100
+# x = 100
 
-dragon = 1
+# dragon = 1
 
-print(dragon)
+# print(dragon)
 
-print(dragon + 1)
+# print(dragon + 1)
 
-just_another_var = dragon * 100
+# just_another_var = dragon * 100
 
-print(just_another_var)
+# print(just_another_var)
+
+i = 2
+
+while i < 6:
+    print(i)
+    i+= 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+def new_function():
+    print("Print this is a function")
+
+new_function()
+
+def another_function(name1, name2, name3):
+    print(name1 + name2 + name3)
+
+another_function("Ronnie ", "Richard ", "Young")
+
+def country_func(country = "Norway"):
+    print("I am from " + country)
+
+country_func("Canada")
+country_func()
+
+def tri_recursion(k):
+    if(k>0):
+        result = tri_recursion(k-1)
+        print(result)
+    else:
+        result = 0
+    return result
+
+tri_recursion(6)

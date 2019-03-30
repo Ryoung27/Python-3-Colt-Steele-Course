7ffbf04017664034fe3b79334a2ca4bf64550d98
Ryoung27 <RRYoung89@gmail.com>
2019-03-29 19:19:18 -0500

    objects

---
+++ vars/class.py
@@ -0,0 +1,35 @@
+class my_class:
+    x = 5
+    y = 10
+
+
+p1 = my_class()
+print(my_class.y)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("Richie", 29)
+
+print(p1.name)
+print(p1.age)
+
+
+class Car:
+    def __init__(self, make, age):
+        self.make = make
+        self.age = age
+
+    def myfunc(self):
+        print("We own a " + self.make)
+
+toyota = Car("Toyota", "new")
+print(toyota.age)
+pontiac = Car("Pontiac", "older")
+print(pontiac.make)
+
+
+
+pontiac.make

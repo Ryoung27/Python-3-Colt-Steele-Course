0d860d1741794e87761f624f903bf620c2845d2b
Ryoung27 <RRYoung89@gmail.com>
2019-03-31 13:47:11 -0500

    lucky_number_two

--- converter/lucky_number.py
+++ converter/lucky_number.py
@@ -6,3 +6,12 @@ if choice == 7:
     print("Lucky")
 else:
     print("Not lucky")
+
+num = randint(1,1000)
+
+if num % 2 == 0:
+    print("Even")
+    print(num)
+else:
+    print("Odd")
+    print(num)

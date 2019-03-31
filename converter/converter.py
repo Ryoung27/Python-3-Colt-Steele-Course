a69eb27b07d3f8ac07f29dfd00fc7b3319b1a10b
Ryoung27 <RRYoung89@gmail.com>
2019-03-31 11:48:45 -0500

    converter

---
+++ converter/converter.py
@@ -0,0 +1,10 @@
+milage = input("How many miles did you run today? ")
+
+milage = float(milage)
+
+kilometers = (milage * 1.60934)
+milage = str(milage)
+kilometers = str(kilometers)
+print("If you ran " + milage + " miles, that is " + kilometers + " kilometers.")
+# print(milage)
+print(kilometers)

ef825dcb50e71e6edf8fbc3323898495161c4480
Ryoung27 <RRYoung89@gmail.com>
2019-04-01 07:16:38 -0500

    more slow down

--- converter/cond.py
+++ converter/cond.py
@@ -1,8 +1,11 @@
-name ="No I don't"
+name ="Young"
 
 if name == "Ronnie":
     print("Hey Ronnie")
 elif name == "Richie":
     print("Hi Richie")
+elif name == "Young":
+    print("What is this freshman english?")
 else:
     print("Do you know how to code?")
+

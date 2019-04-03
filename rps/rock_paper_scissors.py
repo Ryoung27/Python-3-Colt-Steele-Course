ef9b08bc21f185917f4928a980dd892fa2cd9dff
Ryoung27 <RRYoung89@gmail.com>
2019-04-02 19:22:04 -0500

    finished

--- rps/rock_paper_scissors.py
+++ rps/rock_paper_scissors.py
@@ -2,10 +2,11 @@ import random
 
 
 
-computer_input = random.randint(0,2)
+
 
 def rps():
     user_input = str(input("What would you like to have (R)ock, (P)aper, or (S)cissors? "))
+    computer_input = random.randint(0,2)
     if user_input not in ("R", "S", "P", "r", "s", "p") :
         print("Invalid input")
     elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 0:

5262579642a186d8ec08a3a2ccd542b11f2550ea
Ryoung27 <RRYoung89@gmail.com>
2019-04-02 19:07:57 -0500

    refactored rps

--- rps/rock_paper_scissors.py
+++ rps/rock_paper_scissors.py
@@ -1,5 +1,4 @@
 import random
-print(random.randint(0,3))
 
 user_input = str(input("What would you like to have (R)ock, (P)aper, or (S)cissors? "))
 
@@ -12,39 +11,9 @@ computer_input = random.randint(0,2)
 
 if user_input not in ("R", "S", "P", "r", "s", "p") :
     print("Invalid input")
-elif user_input == "R" and computer_input == 0:
+elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 0:
     print("You win!")
-elif user_input == "R" and computer_input == 1:
+elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 1:
     print("You lose.")
-elif user_input == "R" and computer_input == 2:
-    print("Draw")
-elif user_input == "S" and computer_input == 0:
-    print("You win!")
-elif user_input == "S" and computer_input == 1:
-    print("You lose.")
-elif user_input == "S" and computer_input == 2:
-    print("Draw")
-elif user_input == "P" and computer_input == 0:
-    print("You win!")
-elif user_input == "P" and computer_input == 1:
-    print("You lose.")
-elif user_input == "P" and computer_input == 2:
-    print("Draw")
-elif user_input == "r" and computer_input == 0:
-    print("You win!")
-elif user_input == "r" and computer_input == 1:
-    print("You lose.")
-elif user_input == "r" and computer_input == 2:
-    print("Draw")
-elif user_input == "s" and computer_input == 0:
-    print("You win!")
-elif user_input == "s" and computer_input == 1:
-    print("You lose.")
-elif user_input == "s" and computer_input == 2:
-    print("Draw")
-elif user_input == "p" and computer_input == 0:
-    print("You win!")
-elif user_input == "p" and computer_input == 1:
-    print("You lose.")
-elif user_input == "p" and computer_input == 2:
+elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 2:
     print("Draw")

136fd6773108fb30697f9b19bd005c4b0d911fbe
Ryoung27 <RRYoung89@gmail.com>
2019-04-02 19:15:11 -0500

    finished rps

--- rps/rock_paper_scissors.py
+++ rps/rock_paper_scissors.py
@@ -1,19 +1,26 @@
 import random
 
-user_input = str(input("What would you like to have (R)ock, (P)aper, or (S)cissors? "))
+
 
 computer_input = random.randint(0,2)
 
-# if "a" != "b":
-#     print("Worked")
-# else:
-#     print("NO")
+def rps():
+    user_input = str(input("What would you like to have (R)ock, (P)aper, or (S)cissors? "))
+    if user_input not in ("R", "S", "P", "r", "s", "p") :
+        print("Invalid input")
+    elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 0:
+        print("You win!")
+    elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 1:
+        print("You lose.")
+    elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 2:
+        print("Draw")
+    play_again = str(input("Do you want to play again? (Y/N) "))
+    if play_again in ("Y", "y"):
+        rps()
+    else:
+        print("Thanks for playing.")
 
-if user_input not in ("R", "S", "P", "r", "s", "p") :
-    print("Invalid input")
-elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 0:
-    print("You win!")
-elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 1:
-    print("You lose.")
-elif user_input in ("R", "S", "P", "r", "s", "p") and computer_input == 2:
-    print("Draw")
+
+rps()
+
+

08bccc3a33f75e5f76f6b6d4eb97c0207e8f7623
Ryoung27 <RRYoung89@gmail.com>
2019-04-06 17:27:03 -0500

    list challenge

--- list/list.py
+++ list/list.py
@@ -1,9 +1,20 @@
-nums = []
+# nums = []
 
-x = 0
-for i in range(1, 100):
-    nums.append(i)
-    x = x + i
+# x = 0
+# for i in range(1, 100):
+#     nums.append(i)
+#     x = x + i
 
-print(x)
-print(nums)
+# print(x)
+# print(nums)
+
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+
+people.pop(0)
+people.insert(0, "Hannah")
+people.pop(4)
+people.insert(4, "Jeffrey")
+people.pop()
+people.append("Aparna")
+
+print(people)

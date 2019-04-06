d025f75e4c34965e49e768a7b644b638984d1ba0
Ryoung27 <RRYoung89@gmail.com>
2019-04-06 07:57:07 -0500

    logic

---
+++ converter/logic.py
@@ -0,0 +1,17 @@
+age = 6
+
+age > 2
+
+age < 8
+
+age > 2 and age < 8
+
+if age > 2 and age < 8:
+    print("You pay for the ticket.")
+
+city = input("Where do you live?")
+
+if city == "nashville" or city == "knoxville":
+    print("You live in Tennessee")
+else:
+    print("Poor you")

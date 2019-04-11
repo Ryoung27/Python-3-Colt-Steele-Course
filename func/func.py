eacf258b8dd11b516a9d2cf60e9e8edcc5f4a97b
Ryoung27 <RRYoung89@gmail.com>
2019-04-10 20:40:07 -0500

    function test one

--- func/func.py
+++ func/func.py
@@ -1,52 +1,57 @@
-def make_noise():
-    print("hello")
+# def make_noise():
+#     print("hello")
 
-make_noise()
+# make_noise()
 
-def speak_pig():
-    return "oink"
+# def speak_pig():
+#     return "oink"
 
-speak_pig()
+# speak_pig()
 
-list_even = []
-def generate_evens():
-    for i in range(1,50):
-        if i % 2 == 0:
-            list_even.append(i)
-    print(list_even)
+# list_even = []
+# def generate_evens():
+#     for i in range(1,50):
+#         if i % 2 == 0:
+#             list_even.append(i)
+#     print(list_even)
 
-generate_evens()
+# generate_evens()
 
-def yell(scream_it):
-    return scream_it.upper()
+# def yell(scream_it):
+#     return scream_it.upper()
 
-print(yell("ahhhh"))
+# print(yell("ahhhh"))
 
-def speak(animal):
-    if animal == "pig":
-        return "Oink"
-    elif animal == "duck":
-        return "Quack"
-    elif animal == "cat":
-        return "Meow"
-    elif animal == None:
-        animal == "dog"
-    elif animal == "dog":
-        return "Bark"
-    else:
-        return "???"
+# def speak(animal):
+#     if animal == "pig":
+#         return "Oink"
+#     elif animal == "duck":
+#         return "Quack"
+#     elif animal == "cat":
+#         return "Meow"
+#     elif animal == None:
+#         animal == "dog"
+#     elif animal == "dog":
+#         return "Bark"
+#     else:
+#         return "???"
 
-print(speak("dog"))
-print(speak("cat"))
-print(speak("dog"))
-print(speak("m"))
+# print(speak("dog"))
+# print(speak("cat"))
+# print(speak("dog"))
+# print(speak("m"))
 
-def count_dollar_signs(word):
-    count = 0
-    for char in word:
-        if char == '$':
-            count += 1
-    return count
+# def count_dollar_signs(word):
+#     count = 0
+#     for char in word:
+#         if char == '$':
+#             count += 1
+#     return count
 
-print(count_dollar_signs("$uper $ize"))
+# print(count_dollar_signs("$uper $ize"))
 
+def product(x, y):
+    return(x * y)
+
+print(product(1, 1))
+print(product(7, 10))

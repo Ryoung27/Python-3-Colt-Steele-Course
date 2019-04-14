fce3268a9479065bd4382dec0db8fb06574660f2
Ryoung27 <RRYoung89@gmail.com>
2019-04-14 09:08:57 -0500

    kwargs

--- modules/kwarg.py
+++ modules/kwarg.py
@@ -4,3 +4,16 @@ def fav_colors(**kwargs):
         print(f"{person}'s favorite color is {color}'")
 
 fav_colors(Richie="Purple", ruby="red", Colt="Purple")
+
+# def combine_words(**kwargs):
+#     for i, prefix in kwargs.items():
+#         print(f"{i}{prefix}")
+
+# combine_words("child", prefix="ish")
+
+def combine_words(word, **kwargs):
+    if 'prefix' in kwargs:
+        return kwargs['prefix'] + word
+    elif 'suffix' in kwargs:
+        return word + kwargs['suffix']
+    return word

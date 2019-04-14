0db76e95b8818515f6a4999ca0953f87a5ed9071
Ryoung27 <RRYoung89@gmail.com>
2019-04-14 09:21:14 -0500

    lambda cube

---
+++ lam/lambda.py
@@ -0,0 +1,14 @@
+# def square(num):
+#     return num * num
+# print(square(9))
+
+square2 = lambda num: num * num
+print(square2(9))
+
+# add = lambda a,b: a + bin
+
+cube = lambda a: a **3
+
+print(cube(2))
+print(cube(3))
+print(cube(4))

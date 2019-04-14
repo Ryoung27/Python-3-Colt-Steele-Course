f5da98d9fc8618ff96ffb9db74ea70bd3e713f0d
Ryoung27 <RRYoung89@gmail.com>
2019-04-14 08:46:18 -0500

    is keyword

--- modules/mod.py
+++ modules/mod.py
@@ -1,4 +1,27 @@
-import random
+# import random as rand
+import keyword
+from math import sqrt
+from random import choice, randint
 
-print(random.choice(["Ronnie", "Richard", "Young"]))
-print(random.randint(1, 100))
+print(choice(["Ronnie", "Richard", "Young"]))
+print(randint(1, 100))
+
+answer = sqrt(15129)
+print(answer)
+
+# def contains_keywords(*args):
+#     for item in args:
+#         if item.iskeyword(item):
+#             return True
+#         else:
+#             return False
+
+# contains_keywords("hello", "goodbye")
+
+def contains_keyword(*args):
+    for item in args:
+        if keyword.iskeyword(item): return True
+    return False
+
+answer = contains_keyword("Hello", "goodbye")
+print(answer)

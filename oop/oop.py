2fd55db7e8ed81e6049f4772eae2009c9c50064e
Ryoung27 <RRYoung89@gmail.com>
2019-04-17 06:34:04 -0500

    abstraction and encapsulation

--- oop/oop.py
+++ oop/oop.py
@@ -1,19 +1,27 @@
-class Comment:
-    def __init__(self, username, text, likes=0):
-        self.username = username
-        self.text = text
-        self.likes = likes
+# class Comment:
+#     def __init__(self, username, text, likes=0):
+#         self.username = username
+#         self.text = text
+#         self.likes = likes
 
-class Vechicle:
-    pass
+# class Vechicle:
+#     pass
 
-car = Vechicle()
-boat = Vechile()
+# car = Vechicle()
+# boat = Vechile()
 
-class User:
-    pass
+# class User:
+#     pass
 
-user1 = User()
+# user1 = User()
 
-def add:
-    return 3
+# def add:
+#     return 3
+
+deck = Deck()
+deck.shuffle()
+hand1 = deck.deal_hand(5)
+hand2 = deck.deal_hand(5)
+print(hand1)
+print(hand2)
+deck.reset()

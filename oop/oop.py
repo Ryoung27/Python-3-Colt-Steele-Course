bf0fab04ed660c00566a4907e584d2618d4ec503
Ryoung27 <RRYoung89@gmail.com>
2019-04-16 19:35:24 -0500

    Comment

--- oop/oop.py
+++ oop/oop.py
@@ -1,3 +1,9 @@
+class Comment:
+    def __init__(self, username, text, likes=0):
+        self.username = username
+        self.text = text
+        self.likes = likes
+
 class Vechicle:
     pass
 

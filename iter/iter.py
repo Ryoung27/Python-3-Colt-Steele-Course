5f8940a975cff183ddf30deb5e59d72d53a40e4e
Ryoung27 <RRYoung89@gmail.com>
2019-04-18 16:08:44 -0500

    iteration

--- iter/iter.py
+++ iter/iter.py
@@ -1,5 +1,29 @@
-name = "Oprah"
-next(name)
+# name = "Oprah"
+# next(name)
 
-it = iter(name)
-next(it)
+# it = iter(name)
+# next(it)
+
+#Custom for loop
+# for num in [1,2,3]
+# for char in "hi there"
+
+# iter()
+# next()
+# next()
+
+def my_for(iterable, func):
+    iterator = iter(iterable)
+    # print(next(iterator))
+    # print(next(iterator))
+    while True:
+        try:
+            thing = next(iterator)
+        except StopIteration:
+            print("End of iterator.")
+            break
+        else:
+            func(thing)
+
+my_for("hello", print)
+my_for([1,2,3,4], print)

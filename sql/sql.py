b84e41bd314f48cfe1c7831cfa46b08c4d440035
Ryoung27 <RRYoung89@gmail.com>
2019-04-18 13:23:23 -0500

    insert

--- sql/sql.py
+++ sql/sql.py
@@ -9,3 +9,12 @@ CREATE TABLE cats (
     breed TEXT,
     age INTEGER
 );
+
+INSERT INTO table_name (name, breed, age) VALUES
+("Blue", "Random", 29);
+
+INSERT INTO table_name (values in table in order) VALUES
+(values match the order)
+
+SELECT * FROM table_name
+

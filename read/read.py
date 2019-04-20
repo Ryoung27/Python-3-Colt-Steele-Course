8677f6678af219fd11cd41821108a1590a46f1bb
Ryoung27 <RRYoung89@gmail.com>
2019-04-20 15:16:45 -0500

    writing to files

--- read/read.py
+++ read/read.py
@@ -1,5 +1,22 @@
-f =open("text.txt")
+# f =open("text.txt")
 
-print(f)
+# print(f)
 
-print(f.read())
+# print(f.read())
+
+# # print(f.seek(1))
+# # file.read()
+
+# f.closed()
+
+with open("text.txt") as f:
+    data = f.read()
+    print(data)
+
+with open("haiku.txt", "w") as r:
+    r.write("Writing files is interesting.\n")
+    r.write("One more time.")
+
+with open("haiku.txt") as r:
+    difdata = r.read()
+    print(difdata)

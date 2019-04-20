b2567a1d00ecde5076102203f3890a0b28efb1fa
Ryoung27 <RRYoung89@gmail.com>
2019-04-20 08:46:17 -0500

    gen

--- iter/iter.py
+++ iter/iter.py
@@ -12,18 +12,40 @@
 # next()
 # next()
 
-def my_for(iterable, func):
-    iterator = iter(iterable)
-    # print(next(iterator))
-    # print(next(iterator))
-    while True:
-        try:
-            thing = next(iterator)
-        except StopIteration:
-            print("End of iterator.")
-            break
-        else:
-            func(thing)
+# def my_for(iterable, func):
+#     iterator = iter(iterable)
+#     # print(next(iterator))
+#     # print(next(iterator))
+#     while True:
+#         try:
+#             thing = next(iterator)
+#         except StopIteration:
+#             print("End of iterator.")
+#             break
+#         else:
+#             func(thing)
 
-my_for("hello", print)
-my_for([1,2,3,4], print)
+# my_for("hello", print)
+# my_for([1,2,3,4], print)
+
+class Counter:
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.high:
+            num = self.current
+            self.current += 1
+            return num
+
+
+
+c = Counter(0, 10)
+iter(c)
+
+for x in Counter(0,4):
+    print(x)

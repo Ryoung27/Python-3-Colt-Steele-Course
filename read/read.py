4efcd9b88b6cc2a54cf4a833257a3b480714c062
Ryoung27 <RRYoung89@gmail.com>
2019-04-20 15:27:18 -0500

    making copies

--- read/read.py
+++ read/read.py
@@ -23,3 +23,12 @@ with open("haiku.txt") as r:
 
 with open("haiku.txt", "r+") as r:
     r.write("Look a different line.\n")
+
+def copy(file_name, new_file_name):
+    with open(file_name) as file:
+        text = file.read()
+
+    with open(new_file_name, "w") as new_file:
+        new_file.write(text)
+
+copy("text.txt", "new.txt")

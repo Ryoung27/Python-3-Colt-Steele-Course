f0d4acb680f12f2e25e4ba206a7c80d6a5fd3cdc
Ryoung27 <RRYoung89@gmail.com>
2019-04-20 08:01:20 -0500

    insertion

--- sql/friends.py
+++ sql/friends.py
@@ -6,7 +6,16 @@ conn = sqlite3.connect("my_friends.db")
 #CREATE CURSOR OBJECT
 c = conn.cursor()
 #EXECUTE SOME SQL
-c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT closeness INTEGER);")
+# c.execute("CREATE TABLE friends (first_name TEXT, last_name TEXT, closeness INTEGER);")
+# insert_query = '''INSERT INTO friends VALUES ('Merriwether', 'Lewis', 7)'''
+
+# form_first = "Dana"
+
+#BETTER way
+data = ("Steve", "Irwin", 9)
+query = f"INSERT INTO friends VALUES (?,?,?)"
+
+c.execute(query, data)
 #COMMIT CHANGES
 conn.commit()
 conn.close()
